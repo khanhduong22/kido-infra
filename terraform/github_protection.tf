e044bbd79762d83b52c240b5eea9ae037cfbec3d
KhanhDP <phuckhanh22@gmail.com>
2026-04-28 16:17:15 +0700

    chore: remove PR requirement to allow direct push to main

--- terraform/github_protection.tf
+++ terraform/github_protection.tf
@@ -3,19 +3,13 @@ resource "github_branch_protection" "badminton_main" {
   repository_id = "danang-badminton-hub"
   pattern       = "main"
 
-  # Bắt buộc Pull Request
-  required_pull_request_reviews {
-    required_approving_review_count = 0 # Có thể tăng lên 1 nếu có team member
-    require_code_owner_reviews      = false
-  }
-
-  # Bắt buộc CI/CD Pass
+  # Bắt buộc CI/CD Pass (Ví dụ: phải pass action 'build')
   required_status_checks {
     strict   = true
     contexts = ["build"] # TODO: Đổi thành tên job thực tế của GitHub Action (ví dụ: 'build', 'test')
   }
 
-  enforce_admins = true # Bắt buộc admin cũng phải tuân thủ luật
+  enforce_admins = false # Tạm thời nới lỏng cho Admin (cho phép bạn push thẳng)
 }
 
 # --- Branch Protection cho kido-infra ---
@@ -23,9 +17,5 @@ resource "github_branch_protection" "infra_main" {
   repository_id = "kido-infra"
   pattern       = "main"
 
-  required_pull_request_reviews {
-    required_approving_review_count = 0
-  }
-
-  enforce_admins = true
+  enforce_admins = false # Tạm thời nới lỏng cho Admin
 }
