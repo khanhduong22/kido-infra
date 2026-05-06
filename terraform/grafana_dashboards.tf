bb3d8ea87f1417bcdbe12bc776e910921035e217
KhanhDP <phuckhanh22@gmail.com>
2026-05-06 09:19:27 +0700

    fix: comment out invalid grafana dashboard resource

--- terraform/grafana_dashboards.tf
+++ terraform/grafana_dashboards.tf
@@ -1,4 +1,4 @@
-resource "grafana_dashboard" "kido_ops" {
-  config_json = file("${path.module}/dashboards/kido_ops.json")
-  overwrite   = true
-}
+# resource "grafana_dashboard" "kido_ops" {
+#   config_json = file("${path.module}/dashboards/kido_ops.json")
+#   overwrite   = true
+# }
